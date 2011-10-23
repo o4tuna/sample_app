90c34a11439534d59fb39e7557a17295b505b56b
Richard Sneed <sneed.richard@gmail.com>
2011-10-22 23:42:40 -0500

    User model with passwords, and user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ SampleApp::Application.routes.draw do
 
   resources :users
 
+  get "users/new"
+
   match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
@@ -73,7 +75,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
